931632f519c501cac01d411bd5205e6916bb530b
Jkerdley <jk-erley@yandex.ru>
2024-10-11 22:50:46 +0700

    add POST and delete with json server

--- src/components/Todolist/Todolist.jsx
+++ src/components/Todolist/Todolist.jsx
@@ -5,35 +5,63 @@ import styles from './Todo.module.css';
 export const TodoList = () => {
 	const [title, setTitle] = useState('');
 	const [todos, setTodos] = useState([]);
+	const [loading, setLoading] = useState(false);
+	const [deleteItem, setDeleteItem] = useState(false);
+	const [refreshTodos, setRefreshTodos] = useState(false);
+
+	const refreshTodoList = () => setRefreshTodos(!refreshTodos);
 
 	useEffect(() => {
-		fetch('https://mocki.io/v1/dfd2d0b1-46be-4be3-b976-254fc2da22a3').then((data) => {
-			data.json().then((todosData) => {
-				setTodos([...todos, ...todosData]);
-			});
-		});
-	}, []);
+		setLoading(true);
+		fetch('http://localhost:3004/todos')
+			.then((loadedData) => loadedData.json())
+			.then((loadedTodos) => {
+				setTodos(loadedTodos);
+			})
+			.finally(() => setLoading(false));
+	}, [refreshTodos]);
 
-	const addTodo = () => {
+	const requestAddTodo = () => {
 		if (title) {
-			let newTodo = {
-				id: Date.now(),
-				title: title,
-			};
-			setTodos([...todos, newTodo]);
+			fetch(`http://localhost:3004/todos`, {
+				method: 'POST',
+				headers: { 'Content-type': 'application/json; charset=utf-8' },
+				body: JSON.stringify({
+					title: title,
+					completed: false,
+				}),
+			})
+				.then((raw) => raw.json())
+				.then((response) => {
+					console.log('Задача добавлена', response);
+					refreshTodoList();
+				});
 			setTitle('');
 		} else if (!title) {
 			alert('Пожалуйста введите текст задачи');
 		}
 	};
 
-	const deleteTodoItem = (deletedItemId) => {
-		setTodos(todos.filter((todo) => todo.id !== deletedItemId));
+	const requestDeleteTodoItem = (id) => {
+		if (!id) {
+			console.error('Идентификатор задачи не указан');
+			return;
+		}
+		setDeleteItem(true);
+		fetch(`http://localhost:3004/todos/${id}`, {
+			method: 'DELETE',
+		})
+			.then((raw) => raw.json())
+			.then((response) => {
+				console.log('Задача удалена', response);
+				refreshTodoList();
+			})
+			.finally(() => setDeleteItem(false));
 	};
 
 	const handleKeyPress = (event) => {
 		if (event.key === 'Enter') {
-			addTodo();
+			requestAddTodo();
 		}
 	};
 
@@ -48,15 +76,19 @@ export const TodoList = () => {
 				placeholder="Введите задачу"
 				onKeyDown={handleKeyPress}
 			/>
-			<button onClick={addTodo} className={styles.button}>
+			<button onClick={requestAddTodo} className={styles.button}>
 				{' '}
 				Добавить{' '}
 			</button>
 
 			<ul>
-				{todos.map((todo) => (
-					<Item key={todo.id} title={todo.title} id={todo.id} deleteTodoItem={deleteTodoItem} />
-				))}
+				{loading ? (
+					<div className={styles.loader} />
+				) : (
+					todos.map((todo) => (
+						<Item key={todo.id} title={todo.title} id={todo.id} deleteTodoItem={requestDeleteTodoItem} />
+					))
+				)}
 			</ul>
 		</div>
 	);
